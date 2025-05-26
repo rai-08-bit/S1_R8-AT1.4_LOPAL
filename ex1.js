1df627b5dd7b89e8689b43b4503107ce16f49e96
rai-08-bit <raissa.bez08@outlook.com>
2025-05-26 16:09:29 -0300

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,13 +1,13 @@
-let idades = [];
-let idade;
+let idades = []; // criação da array "idades" para armazenar valores das idades digitadas pelo usuário
+let idade; // criação da variável "idade" para armazenar separadamente o valor posição
 
 for (let i = 0; i < 4; i++) {
 
     do {
-        idade = parseFloat(prompt(`Digite a idade ${i + 1}`));
+        idade = parseFloat(prompt(`Digite a idade ${i + 1}`)); // Caixa de texto para que o usuário digite a idade de acordo com a posição
 
-        if (isNaN(idade) || idade < 0 || idade > 100) {
-            alert("Idade inválida, digite um número entre 0 e 100");
+        if (isNaN(idade) || idade < 0 || idade > 100) { //Verifica se o valor digitado é verdadeiro
+            alert("Idade inválida, digite um número entre 0 e 100"); //
         }
     } while (isNaN(idade) || idade < 0 || idade > 100);
 
