bf83ec63444bdd8872ea69aa52811b14f16a153f
rai-08-bit <raissa.bez08@outlook.com>
2025-06-02 12:49:51 -0300

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -1,14 +1,13 @@
-<<<<<<< HEAD
-let idades = []; //
-let idade; //
-let posicaoMaior; //
+let idades = []; // criação de array "idades" para armazenar diversos valores da idade das 4 pessoas
+let idade; // criação de variável "idade" para armazenar separadamente os valores da idade das 4 pessoas
+let posicaoMaior; // criação da variável "posicaoMaior" para
 for (let i = 0; i < 4; i++) {
-
+// inicio do looping for para que possa se repetir 4 vezes a mesma página
     do {
-        idade = parseFloat(prompt(`Digite a idade ${i + 1}`)); //
+        idade = parseFloat(prompt(`Digite a idade ${i + 1}`));
 
         if (isNaN(idade) || idade < 0 || idade > 100) {
-=======
+            
 let idades = []; // criação da array "idades" para armazenar valores das idades digitadas pelo usuário
 let idade; // criação da variável "idade" para armazenar separadamente o valor posição
 
@@ -18,31 +17,28 @@ for (let i = 0; i < 4; i++) {
         idade = parseFloat(prompt(`Digite a idade ${i + 1}`)); // Caixa de texto para que o usuário digite a idade de acordo com a posição
 
         if (isNaN(idade) || idade < 0 || idade > 100) { //Verifica se o valor digitado é verdadeiro
->>>>>>> 1df627b5dd7b89e8689b43b4503107ce16f49e96
-            alert("Idade inválida, digite um número entre 0 e 100"); //
-        }
-    } while (isNaN(idade) || idade < 0 || idade > 100); //
 
-    idades[i] = idade; //
+            alert("Idade inválida, digite um número entre 0 e 100");
+        }
+    } while (isNaN(idade) || idade < 0 || idade > 100);
+
+    idades[i] = idade; 
 }
 
-let maiorIdade = idades[0]; //
-let soma = 0; //
+let maiorIdade = idades[0]; 
+let soma = 0; 
 
 for (let i = 0; i < 4 ; i++) {
-    soma += idades[i]; //
+    soma += idades[i]; 
 
     //Verificando idades
     if (idades[i] > maiorIdade) {
-        maiorIdade = idades[i]; //
-        posicaoMaior = i; //
+        maiorIdade = idades[i]; 
+        posicaoMaior = i; 
     }
 }
 
-<<<<<<< HEAD
 alert(`A maior idade é: ${maiorIdade}`); //
 alert (`A posição em que ela foi digitada é: ${posicaoMaior}`); //
 =======
 alert(`A maior idade é: ${maiorIdade}`);
-console.log
->>>>>>> 1df627b5dd7b89e8689b43b4503107ce16f49e96
